aae4171ab63c0827a21e4b59a49c811b3c4838e4
unknown <cyberbrodo8@gmail.com>
2026-07-01 01:23:14 +0530

    Use Render environment variables

--- mypro/settings.py
+++ mypro/settings.py
@@ -131,12 +131,18 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+import os
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='quickjobs073@gmail.com'
-EMAIL_HOST_PASSWORD ='htfn ofwd vpkw xelf'
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_TIMEOUT = 10
 
 
 
