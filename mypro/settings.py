c4a48423a869f80f4387e4dfd461f5d687a7f605
unknown <cyberbrodo8@gmail.com>
2026-08-06 02:05:03 +0530

    up

--- mypro/settings.py
+++ mypro/settings.py
@@ -66,6 +66,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 ROOT_URLCONF = 'mypro.urls'
 
 TEMPLATES = [
@@ -143,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
